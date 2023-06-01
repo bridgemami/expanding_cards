0f1e7a739c1b1cdfad11243f5ce6ef5105823b31
bridgemami <mdbridgeman@gmail.com>
2023-05-31 17:35:59 -0700

    added notes and changed the transition

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cards = document.querySelectorAll('.panel');
+//remove all the active class
 const removeActiveClasses= ()=> {
     cards.forEach(card => {
      card.classList.remove('active');   
     })
     }
+    //first remove all active classes before addinging it
 cards.forEach((card)=> {
         card.addEventListener('click', ()=> {
             removeActiveClasses();
